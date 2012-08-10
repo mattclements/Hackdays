700a01098c2b5c8ab1ae808bce2157e3a7890a6f
Matt Clements <matt@mattclements.co.uk>
2012-08-10 21:10:54 +0100

    Added Sample Configuration File

---
+++ includes/configure.sample.inc.php
@@ -0,0 +1,14 @@
+<?php
+/**
+*	Rename this file to configure.inc.php to setup the site
+*
+*/
+define('DBHOST','localhost');
+define('DBPORT','3306');
+
+define('DBNAME','');
+define('DBUSER','');
+define('DBPASS','');
+
+
+?>
