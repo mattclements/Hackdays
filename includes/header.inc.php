d9344546aa7743f28fa772d60f248ec8874a5cef
Matt Clements <matt@mattclements.co.uk>
2012-08-10 21:36:42 +0100

    Adding extra feature

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -6,8 +6,8 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <title>Upcoming Hackdays near you!</title>
-  <meta name="description" content="">
+  <title>Upcoming Hackdays near you</title>
+  <meta name="description" content="Bringing details of upcoming hackdays near you. Hackdays, Geek Meets and Conferences for Technology">
 
   <meta name="viewport" content="width=device-width">
   
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+include($_SERVER['DOCUMENT_ROOT'].'/includes/configure.inc.php');
 include($_SERVER['DOCUMENT_ROOT'].'/includes/header.inc.php');
 ?>
 
