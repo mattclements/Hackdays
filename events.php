ccc73d83777d021bc76ed861a74e4d73f34cb090
Matt Clements <matt@mattclements.co.uk>
2012-08-13 16:32:20 +0100

    Added Events Page

---
+++ events.php
@@ -0,0 +1,10 @@
+<?php
+$page="organisers";
+include_once($_SERVER['DOCUMENT_ROOT'].'/includes/session.inc.php');
+include_once($_SERVER['DOCUMENT_ROOT'].'/includes/header.inc.php');
+?>
+
+<h1>Upcoming Events</h1>
+<?php
+include_once($_SERVER['DOCUMENT_ROOT'].'/includes/footer.inc.php');
+?>
